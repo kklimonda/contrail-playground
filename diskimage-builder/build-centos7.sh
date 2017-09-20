3de1af5e82f12fd427b744122a3d10a0a33d6c1b
Jarek Łukow <jaroslaw.lukow@codilime.com>
2017-09-20 11:05:14 +0200

    minor usability changes

--- diskimage-builder/build-centos7.sh
+++ diskimage-builder/build-centos7.sh
@@ -1,8 +1,9 @@
 export DIB_RELEASE=7.1.1503
-export DIB_LOCAL_IMAGE=/home/cloud-user/CentOS-7-x86_64-GenericCloud-1503.qcow2
+export DIB_LOCAL_IMAGE="${1:-./CentOS-7-x86_64-GenericCloud-1503.qcow2}"
+export VENV_PATH="${2:-./venv}"
 export DIB_DISTRIBUTION_MIRROR=http://vault.centos.org
 export DISTRO=centos7
 export ELEMENTS_PATH=elements/
 
-source /opt/dib/bin/activate
+source "${VENV_PATH}"/bin/activate
 bash -x build-image.sh
